a7d2adc6a026c8659c80b5bdfdf8f973692e468e
unknown <nguyentuan24072015@gmail.com>
2023-01-12 16:57:26 +0700

    B1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle;
+        circle = new Circle(10, "yellow");
+        //System.out.println(circle);
+        Cylinder cylinder;
+        cylinder = new Cylinder(20, 10, "Green");
+        System.out.println(cylinder);
+    }
+}
